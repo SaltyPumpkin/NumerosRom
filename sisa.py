5093b7efb89a9c0765b7b8c359731bb67781c18a
Juan_David_Martienz_S <juand.martinezs@uqvirtual.edu.co>
2022-05-11 18:45:51 -0500

    first commit

---
+++ sisa.py
@@ -0,0 +1,29 @@
+z = "Ingrese un texto en minuscula con números romanos en mayusculas: "
+a = [["I",1],["V",5],["X",10],["L",50],["C",100]] #siempre en 0 tengo la letra y en 1 tengo el significado
+b = input(z)
+c = b.split() #va a separar por espacios las palabras y las guarda en una lista
+d = "" #las palabras que van a ir en el texto final
+for i in range(len(c)): #va a recorrer cada uno de los elementos, recorre cada una de las palabras
+    if c[i].isupper(): #isupper determina si la cadena de texto es mayuscula o minuscula
+        e = [] #lista auxiliar, guarda equivalencias de los números
+        for j in c[i]: #va a recorrer las letras de las palabras que estan guardadas en i
+            for k in range(len(a)): #Para hacer la busqueda en la lista a
+                if j == a[k][0]: #si la letra en la posicion k_0 es la letra, y 0 es la letra
+                    e.append(a[k][1]) #Aqui busca el número
+                    break 
+        if len(c[i]) == len(e): #el tamaño de la palabra es igual al tamaño de las equivalencias encontradas
+            suma = 0
+            aux = e[0] # se guarda la primera equivalencia 
+            for j_2 in range(1, len(e)):
+                if aux >= e[j_2]:
+                    suma = suma + aux
+                else:
+                    suma = suma - aux
+                aux = e[j_2]
+            suma = suma + aux
+            d = d + " " + str(suma)
+        else: #si el tamaño de la palabra no es igual a las equivalencias, deja la palabra tal y como esta
+            d = d+" "+c[i]
+    else: #si la palabra es minuscula, se deja tal como esta
+        d = d+" "+c[i] #Sin comas porque imprime mal
+print(d)
